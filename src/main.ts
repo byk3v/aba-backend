8a9e906a3e44674097fa8b81a046045ec720c6db
Humberto Garcia Gonzalez <humberto8711@gmail.com>
2021-01-20 10:14:15 -0500

    small changes

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { NestFactory } from '@nestjs/core';
 import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 
-dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
