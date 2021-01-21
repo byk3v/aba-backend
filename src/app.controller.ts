681326110ee1517c8412cc3128eaf0e5b101f449
elsolidurdaneta <solidurdaneta2@gmail.com>
2021-01-20 21:23:10 -0500

    Revisar el refreshToken que está dando problemas

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,20 @@
-import { Controller, Get, UseGuards, Post, Request } from "@nestjs/common";
-import { AppService } from "./app.service";
+import { Controller, Get,UseGuards,Post,Request } from '@nestjs/common';
+import { AppService } from './app.service';
 import { AuthGuard } from "@nestjs/passport";
 import { AuthService } from "./auth/auth.service";
 
 @Controller()
 export class AppController {
-  private authService: AuthService;
+  constructor(private readonly authService: AuthService) {}
 
-  constructor(private readonly appService: AppService) {
-  }
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @UseGuards(AuthGuard("jwt-refreshtoken"))
-  @Post("auth/refreshtoken")
-  async refreshToken(@Request() req) {
+  @UseGuards(AuthGuard('jwt-refreshtoken'))
+  @Post('auth/refreshToken')
+  async refreshToken(@Request() req){
     return await this.authService.login(req.user);
   }
 }
