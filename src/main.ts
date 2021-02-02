87c3b7c3c114e33bea1b72d04c64c4f25cb63d85
elsolidurdaneta <solidurdaneta2@gmail.com>
2021-02-01 19:39:11 -0500

    Some eslint fix

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
-import {initSwagger} from './app.swagger'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// import { initSwagger } from './app.swagger';
 
 dotenv.config();
 const PORT = process.env.PORT || 3001; // asi funciona heroku
@@ -10,22 +11,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
-
-
-
-    ,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
     credentials: true,
   });
 
   app.useGlobalPipes(
-    new ValidationPipe(
-      {
-        whitelist: true,
-      }
-    ),
+    new ValidationPipe({
+      whitelist: true,
+    }),
   );
   await app.listen(PORT);
 }
